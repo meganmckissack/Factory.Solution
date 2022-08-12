244bbd37340e876121761dde0ee6ff1e14181ba0
Megan McKissack <meganmckissack@gmail.com>
2022-08-12 16:47:42 -0700

    update with proper using references

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using Factory.Models;
 
 namespace Factory.Controllers
