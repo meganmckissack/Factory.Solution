fdb4f684225f5500aaf04d544d975ef9b692428a
Megan McKissack <meganmckissack@gmail.com>
2022-08-11 20:41:44 -0700

    updated models for join entities and lasy loading

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -4,8 +4,14 @@ namespace Factory.Models
 {
   public class Engineer
   {
+    public Artist()
+    {
+      this.JoinEngineerMachine = new HashSet<EngineerMachine>();
+    }
+
     public int EngineerId { get; set; }
     public string EngineerName { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEngineerMachine { get; set; }
   }
 }
 
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -4,7 +4,13 @@ namespace Factory.Models
 {
   public class Machine
   {
+    public Machine()
+    {
+      this.JoinEngineerMachine = new HashSet<EngineerMachine>();
+    }
+
     public int MachineId { get; set; }
     public string MachineName { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEngineerMachine { get; set; }
   }
 }
